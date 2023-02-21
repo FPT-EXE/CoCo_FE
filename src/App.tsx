2447e7a2aba71de7c7fc2a68690c36e106e06758
Daniel Ng <dacnguyen9101@gmail.com>
2023-02-21 20:43:59 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+import './assets/styles/app.css'
+import './assets/styles/app.scss'
+
+const App: FC = () => {
+  return <h1 className='text-primary text-4xl font-bold'>Hello world! I am using React</h1>
+}
+
+export default App
